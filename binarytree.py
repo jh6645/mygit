eb673be8d0aa9fb5f52541e5c70152e6f68f682c
jh6645 <jihoon66452@gmail.com>
2024-04-12 08:51:14 +0900

    BinaryTree-Data Structure

--- binarytree.py
+++ binarytree.py
@@ -47,3 +47,9 @@ class BinaryTree:
         if root==None:
             return 0
         return max(self.height(root.left),self.height(root.right))+1
+
+    def size(self,root):
+        if root is None:
+            return 0
+        else:
+            return 1+self.size(root.left)+self.size(root.right)
